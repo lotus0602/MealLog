67259735fb387ccc86d19e69ff6033f1add58835
N <dbflgks0602@gmail.com>
2016-02-25 14:14:21 +0900

    test

--- app/src/main/java/com/n/fragment/InputMealFragment.java
+++ app/src/main/java/com/n/fragment/InputMealFragment.java
@@ -67,7 +67,6 @@ public class InputMealFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
